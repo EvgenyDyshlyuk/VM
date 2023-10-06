4c2b68844479bf004d6b96c47c9c4ff14cb8eec8
Evgeny Dyslyuk <evgeny.dyshlyuk@gmail.com>
2023-10-06 14:16:01 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# %%
+import pandas as pd
+# import train -test split
+
+
+# load gs://cloud-samples-data/ai-platform-unified/datasets/tabular/petfinder-tabular-classification.csv
+df = pd.read_csv('gs://cloud-samples-data/ai-platform-unified/datasets/tabular/petfinder-tabular-classification.csv')
+df
+# %%
